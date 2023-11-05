b2d2d7977fab508efb44b85a6775539eb35906d7
Garrett Gonzales <gggonzo4@yahoo.com>
2023-11-05 01:45:07 -0700

    added working code color system to change the colors of the past present and future on bars

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -22,12 +22,30 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 });
 
-$(".future").css("background-color", "blue");
 
-      function updateDateTime() {
-        const time = new Date();
-        const currentDateTime = time.toLocaleString();
-        document.querySelector('#datetime').textContent = currentDateTime;
+const timeOfDay = ["9","10","11","12","13","14","15", "16", "17", "18", "19"];
+
+function updateDateTime() {
+  const time = new Date();
+  const currentDateTime = time.toLocaleString();
+  document.querySelector('#datetime').textContent = currentDateTime;
+  const currentHour = time.getHours();
+  timeOfDay.forEach(hourOfDay => {
+    console.log(hourOfDay);
+    if (currentHour==hourOfDay) {
+      $(`#hour-${hourOfDay}`).css("background-color", "red");
+      // console.log(`hour-${hourOfDay}`);
+    }
+    if (currentHour > hourOfDay) {
+      // console.log("thisIsThePastHours");
+      // console.log(hour);
+      $("#hour-" + hourOfDay).css("background-color", "gray");
+      // console.log(`hour-${hourOfDay}`);
+    }
+    if (currentHour < hourOfDay) {
+      $(`#hour-${hourOfDay}`).css("background-color", "green");
+    }
+  })
       }
 
       setInterval(updateDateTime, 1000);
@@ -37,3 +55,20 @@ $(".future").css("background-color", "blue");
 const d = new Date();
 let day = weekday[d.getDay()];
 document.getElementById("dayOfTheWeek").innerHTML = day;
+
+const element = document.getElementById("b1");
+element.addEventListener("click", myFunction);
+console.log(element);
+
+function myFunction() {
+  document.getElementById("b1").innerHTML = "Saved";
+}
+
+function createItem() {
+  localStorage.setItem("b1", Date.now());
+}
+
+function readValue() {
+  var x = localStorage.getItem("b1");
+  document.getElementById("b1").innerHTML = x;
+}
