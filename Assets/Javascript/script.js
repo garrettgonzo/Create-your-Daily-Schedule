1f45605cac9b391e2e644f85fbf203e0950869e6
Garrett Gonzales <gggonzo4@yahoo.com>
2023-11-06 18:51:15 -0800

    created localstorage code so the logs are saved and polished up code

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,28 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
-
-
 const timeOfDay = ["9","10","11","12","13","14","15", "16", "17", "18", "19"];
 
 function updateDateTime() {
@@ -31,7 +6,7 @@ function updateDateTime() {
   document.querySelector('#datetime').textContent = currentDateTime;
   const currentHour = time.getHours();
   timeOfDay.forEach(hourOfDay => {
-    console.log(hourOfDay);
+    // console.log(hourOfDay);
     if (currentHour==hourOfDay) {
       $(`#hour-${hourOfDay}`).css("background-color", "red");
       // console.log(`hour-${hourOfDay}`);
@@ -72,3 +47,18 @@ function readValue() {
   var x = localStorage.getItem("b1");
   document.getElementById("b1").innerHTML = x;
 }
+
+var saveButtons = $(".saveBtn")
+saveButtons.on("click", function (event) {
+  console.log(event.target);
+ var note = $(this).siblings(".description").val();
+ var timeOfNote = $(this).siblings(".hour").text();
+ console.log(timeOfNote);
+ localStorage.setItem(timeOfNote, note)
+})
+
+var timeBlocks = $(".time-block")
+timeBlocks.each(function () {
+  console.log($(this));
+})
+
